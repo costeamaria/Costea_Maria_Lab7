14b67b11166c0bb1146d6cbfd0e92791a55109f7
Maria Costea <costea.mari23@gmail.com>
2022-12-06 23:59:31 +0200

    Add Product

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -10,14 +10,21 @@ public partial class ListPage : ContentPage
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
-        slist.Date = DateTime.UtcNow;
-        await App.Database.SaveShopListAsync(slist);
         await Navigation.PopAsync();
     }
     async void OnDeleteButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
-        await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+
 }
